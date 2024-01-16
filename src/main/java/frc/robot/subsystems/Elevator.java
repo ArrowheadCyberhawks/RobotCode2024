56008190b6f0afd35d4e20299d43e154767b0573
Cyberhawks706 <cyberhawks706@gmail.com>
2024-01-15 19:59:12 -0600

    start work on elevator

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,7 +1,15 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lib.frc706.cyberlib.BrushlessSparkWithPID;
 
 public class Elevator extends SubsystemBase {
+private BrushlessSparkWithPID elevatorMotor;
 
+public Elevator(int sparkID, double kP, double kI, double kD, double kFF, double kIz, double maxVel, double maxAcc, double allowedErr) {
+    elevatorMotor = new BrushlessSparkWithPID(0, 0, 0, 0, 0, 0, 0, 0, 0);
+}
+public void setElevatorMotor(double speed) {
+    elevatorMotor.setVel(speed);
+}
 }
