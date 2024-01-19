b25b16a907e7465c0db358f227d849e27801228a
Stippy174 <lkmillikin@gmail.com>
2024-01-18 18:48:13 -0600

    Added elevator command

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,6 +3,9 @@ package frc.robot.subsystems;
 import lib.frc706.cyberlib.BrushlessSparkWithPID;
 import static frc.robot.Constants.ElevatorConstants.*;
 
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
@@ -10,13 +13,16 @@ public class Elevator extends SubsystemBase {
     private BrushlessSparkWithPID elevatorMotor2;
 
     public Elevator() {
-        elevatorMotor1 = new BrushlessSparkWithPID(kElevatorMotor1Port, 1.0, 0, 0, 1.0, 0, BrushlessSparkWithPID.NEO1650_MAXRPM, 2000, 1.0);
-        elevatorMotor2 = new BrushlessSparkWithPID(kElevatorMotor2Port, 1.0, 0, 0, 1.0, 0, BrushlessSparkWithPID.NEO1650_MAXRPM, 2000, 1.0);
+        elevatorMotor1 = new BrushlessSparkWithPID(kElevatorMotor1Port, 1.0, 0, 0, 1.0, 0,
+                BrushlessSparkWithPID.NEO1650_MAXRPM, 2000, 1.0);
+        elevatorMotor2 = new BrushlessSparkWithPID(kElevatorMotor2Port, 1.0, 0, 0, 1.0, 0,
+                BrushlessSparkWithPID.NEO1650_MAXRPM, 2000, 1.0);
         elevatorMotor2.spark.follow(elevatorMotor1.spark, true);
     }
 
     /**
      * Sets the speed of the elevator motors.
+     * 
      * @param speed Speed of the elevator motors in rotations per minute
      */
     public void setElevatorMotor(double speed) {
@@ -25,6 +31,7 @@ public class Elevator extends SubsystemBase {
 
     /**
      * Gets the position of the elevator motor.
+     * 
      * @return Position of the elevator motor in rotations
      */
     public double getElevatorPosition() {
@@ -36,5 +43,9 @@ public class Elevator extends SubsystemBase {
      */
     public void stopElevator() {
         elevatorMotor1.spark.stopMotor();
+    }
+
+    public Command runElevatorCommand(Supplier<Double> speed) {
+        return this.run(() -> this.setElevatorMotor(speed.get())).finallyDo(() -> stopElevator());
     }
 }
